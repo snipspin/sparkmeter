ba9841f123d3671f036e39ce16c65b076f4dfd8b
snipspin <photonenimpuls@gmail.com>
2020-04-16 13:41:01 -0700

    added index.html

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ app.use(express.urlencoded({ extended: false }))
 app.use(ejsLayouts)
 app.use(express.static(__dirname + '/public/'))
 
-
 app.get('*', (req, res) => {
   res.status(404).send({message: "Not found"})
 })
